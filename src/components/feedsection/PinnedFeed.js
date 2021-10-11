6f52544eddef1597387f51967a633a695478b0bf
hatchedland <rajanyadav_20618@aitpune.edu.in>
2021-10-11 16:23:14 +0530

    Added styling

--- src/components/feedsection/PinnedFeed.js
+++ src/components/feedsection/PinnedFeed.js
@@ -1,6 +1,6 @@
 
 import React, {useState } from 'react'
-import { ReadMore, Arrow } from '../icons/Icons';
+import { ReadMore, Arrow, Pin, CloseButton, KnowMoreDark, KnowMore } from '../icons/Icons';
 
 
 export const PinnedFeed = () => {
@@ -8,24 +8,23 @@ export const PinnedFeed = () => {
 
 	return (
 		<ul>
-				<div className="pinned-feed-header"><ReadMore />Important</div>
+				<div className="pinned-feed-header"><Pin />Important</div>
 
 
 		
 
 				<div className="feed-element">
-					<div className="feed-element-header">
-						<ReadMore />
-						<ReadMore />
-						<ReadMore />
-					</div>
+				
 
 					<div className="feed-content-wrapper">
 						<div className="feed-content">
 							<div className="club-name">lorem</div>
 							<div className="title">Lorem ipsum dolor sit amet.</div>
 						</div>
-						<div className="expand"><Arrow /></div>
+						<div className="feed-content-options">
+						<KnowMore />
+						<CloseButton />
+							</div>
 					</div>
 					<div className="feed-description">
 						Lorem ipsum, dolor sit amet consectetur adipisicing elit. At corporis ex dolores pariatur asperiores veniam animi delectus quas nobis maxime!
--- src/components/homepage/HomeContent.js
+++ src/components/homepage/HomeContent.js
@@ -5,90 +5,90 @@ import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
 export const HomeContent = () => {
 	return (
 		<div className="home-content">
+			
+			<div className="greeting-text">
+				Hi, Great to see you here.<span className="type-animation"></span>
+				</div>
 			<div className="site-description">
 
-				<div className="greeting-text">
-					<Hello /> Hi, great to see you here. Getting involved in an extracurricular club could be one of the highlights of your time in College.
-				</div>
 
 				<div className="club-wrapper">
-					<div className="club-tagline">we publish <br />magazines</div><span className="link-to-club"><Link to='/fee/feed'><strong>@Magboard</strong></Link></span>
+					<div className="club-tagline">We publish <br />magazines</div><span className="link-to-club"><Link to='/fee/feed' className="link"><strong>@Magboard</strong></Link></span>
 					<Magazine />
 				</div>
 				<span className="white-space"></span>
 				<div className="club-wrapper">
-					<div className="club-tagline">improve <br />technical</div>abilities<span className="link-to-club"><Link to='/fee/feed'><strong>@Technical Board</strong></Link></span>
+					<div className="club-tagline">Improve <br />technical</div>abilities<span className="link-to-club"><Link to='/fee/feed' className="link"><strong>@Technical Board</strong></Link></span>
 				</div>
 				<span className="white-space"></span>
 				<div className="club-wrapper">
-					<span className="link-to-club"><Link to='/fee/feed'><strong>@Spiritual Club</strong></Link></span>	<div className="club-tagline">may be a <br />step closer</div> to<God />
+					<span className="link-to-club"><Link to='/fee/feed' className="link"><strong>@Spiritual Club</strong></Link></span>	<div className="club-tagline">may be a <br />step closer</div> to<God />
 				</div>
 				<span className="white-space"></span>
 				<div className="club-wrapper">
-					<Dance /> <div className="club-tagline">.groove<br /> fall in </div> &
-					<Heart /> <div className="club-tagline">with <br /> cultural</div> Fests<span className="link-to-club"><Link to='/fee/feed'><strong>@Cultural Board</strong></Link></span>.
+					<Dance /> <div className="club-tagline">Groove<br />Cultural</div><div className="club-tagline">with<br />fests</div> <span className="link-to-club"><Link to='/fee/feed' className="link"><strong>@Cultural Board</strong></Link></span>.
 				</div>
 				<span className="white-space"></span>
 				<div className="club-wrapper">
-					<span className="link-to-club"><Link to='/fee/feed'><strong>@DDQ Club</strong></Link></span>
+					<span className="link-to-club"><Link to='/fee/feed' className="link"><strong>@DDQ Club</strong></Link></span>
 					<Stage />
-					<div className="club-tagline">we welcomes <br />opinions with</div> drama.
+					<div className="club-tagline">We welcome <br />opinions &</div> drama.
 				</div>
 				<span className="white-space"></span>
 				<div className="club-wrapper">
-					<div className="club-tagline">tune into the  <br /> melody of </div>Your
+					<div className="club-tagline">Tune into the  <br /> melody of </div>Your
 					<Heart />
 					<div className="club-tagline">while <br /> using</div>
-					<span className="link-to-club"><Link to='/fee/feed'><strong>@Radio Raga</strong></Link></span><Radio />
+					<span className="link-to-club"><Link to='/fee/feed' className="link"><strong>@Radio Raga</strong></Link></span><Radio />
 					<div className="club-tagline">as a  <br />headstart. </div>
 				</div>
 				<span className="white-space"></span>
 				<div className="club-wrapper">
 					<span className="white-space"></span><Robot />
 					<div className="club-tagline">Robo  <br />war </div>
-					<span className="link-to-club"><Link to='/fee/feed'><strong>@Robotics Club</strong></Link></span>
+					<span className="link-to-club"><Link to='/fee/feed' className="link"><strong>@Robotics Club</strong></Link></span>
 				</div>
 				<span className="white-space"></span>
 				<div className="club-wrapper">
-					<div className="club-tagline">.Fun Fact : Aryabhatt <br /> was also a member of</div>
-					<span className="link-to-club"><Link to='/fee/feed'><strong>@Maths Club</strong></Link></span>.
+					<div className="club-tagline"><strong>Fun Fact:</strong> Aryabhatt <br /> was also a member of</div>
+					<span className="link-to-club"><Link to='/fee/feed' className="link"><strong>@Maths Club</strong></Link></span>.
 				</div>
 				<span className="white-space"></span>
 				<div className="club-wrapper">
-					<div className="club-tagline">Assist and & serve <br />  above ourselves</div>
-					<span className="link-to-club"><Link to='/fee/feed'><strong>@NSS Club</strong></Link></span><Contribution />.
+					<div className="club-tagline">Assist & Serve <br />  above ourselves</div>
+					<span className="link-to-club"><Link to='/fee/feed' className="link"><strong>@NSS Club.</strong></Link></span><Contribution />
 				</div>
 				<span className="white-space"></span>
 				<div className="club-wrapper">
 					<div className="club-tagline">"Stamina <br /> Matters"</div>~
-					<span className="link-to-club"><Link to='/fee/feed'><strong> Sport Club</strong></Link></span><Sports />
+					<span className="link-to-club"><Link to='/fee/feed' className="link"><strong> Sport Club</strong></Link></span><Sports />
 				</div>
 				<span className="white-space"></span>
 				<div className="club-wrapper">
-					<div className="club-tagline">.Love co-<br />ntributing?</div>here
-					<span className="link-to-club"><Link to='/fee/feed'><strong>@ Open Source Club </strong></Link></span>.
+					<div className="club-tagline">Love co-<br />ntributing?</div><Terminal />here
+					<span className="link-to-club"><Link to='/fee/feed' className="link"><strong>@ Open Source Club.</strong></Link></span>
 				</div>
 				<span className="white-space"></span>
 				<div className="club-wrapper">
 					<div className="club-tagline">Chetak now<br /> operates on</div>
 					<Battery /><div className="club-tagline">find <br />out</div>how
-					<span className="link-to-club"><Link to='/fee/feed'><strong>@SAE Club</strong></Link></span>.
+					<span className="link-to-club"><Link to='/fee/feed' className="link"><strong>@SAE Club</strong></Link></span>.
 				</div>
 				<span className="white-space"></span>
 				<div className="club-wrapper">
 					<Leaf /><div className="club-tagline">is <br />lovely,</div> so is
-					<span className="link-to-club"><Link to='/fee/feed'><strong>@Natures Club</strong></Link></span>.
+					<span className="link-to-club"><Link to='/fee/feed' className="link"><strong>@Natures Club</strong></Link></span>.
 				</div>
 				<span className="white-space"></span>
 				<div className="club-wrapper">
-					<div className="club-tagline"> got Stuck <br />in test cases?</div>
-					<span className="link-to-club"><Link to='/fee/feed'><strong>@Coding Club</strong></Link></span>
+					<div className="club-tagline">Got Stuck <br />in test cases?</div>
+					<span className="link-to-club"><Link to='/fee/feed' className="link"><strong>@Coding Club</strong></Link></span>
 					<div className="club-tagline">got <br />your back.</div> :)
 				</div>
 				<span className="white-space"></span>
 				<div className="club-wrapper">
-					<div className="club-tagline">Thats all,the track ends<br />here, continue your ride with</div>
-					<span className="link-to-club"><Link to='/fee/feed'><strong>@Cycling Club</strong></Link></span> <Cycle />.
+					<div className="club-tagline">Thats all, the track ends<br />here, continue your ride with</div>
+					<span className="link-to-club"><Link to='/fee/feed' className="link"><strong>@Cycling Club</strong></Link></span> <Cycle />.
 				</div>
 			</div>
 
